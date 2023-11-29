d8cb56714add154e4203414e1b49378eff1665f9
gocai <gorcai2002@gmail.com>
2023-11-28 18:07:42 -0800

    Health Bars

--- Assets/Script/EnemyScripts/EnemyHealth.cs
+++ Assets/Script/EnemyScripts/EnemyHealth.cs
@@ -1,18 +1,24 @@
 
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
     public int maxHealth = 30;
     private int currentHealth;
 
+    public Image healthBarImage;
+
+    public Gradient healthGradient;
     private ScoreUI ScoreManager;
+    
     void Start()
     {
         // Initialize current health to max health at the start
         currentHealth = maxHealth;
 
         ScoreManager = FindObjectOfType<ScoreUI>();
+        UpdateHealthBar();
     }
 
     // Method to decrease enemy health
@@ -26,6 +32,7 @@ public class EnemyHealth : MonoBehaviour
         {
             Die();
         }
+        UpdateHealthBar();
     }
 
     // Optional: Method to handle enemy death behavior
@@ -35,5 +42,19 @@ public class EnemyHealth : MonoBehaviour
         ScoreManager.UpdateScore(10);
         // Destroy the enemy GameObject
         Destroy(gameObject);
+    }
+
+    
+    void UpdateHealthBar()
+    {
+        // Calculate the fill amount based on the current health
+        float fillAmount = (float)currentHealth / maxHealth;
+
+        // Set the fill amount of the health bar image
+        if (healthBarImage != null)
+        {
+            healthBarImage.fillAmount = fillAmount;
+            healthBarImage.color = healthGradient.Evaluate(1 - fillAmount);
+        }
     }
 }
