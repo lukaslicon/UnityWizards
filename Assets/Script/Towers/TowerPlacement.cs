3a6ebde577c6ff841f85f5e800dcfb2ed9597ab3
lukaslicon <lukas@licons.com>
2023-12-01 11:09:27 -0800

    TowerPlacementRaycast
    Build works and can be played but currently tower button is a work in progress. does not work fully.

---
+++ Assets/Script/Towers/TowerPlacement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlacement : MonoBehaviour
+{
+    [SerializeField] private Camera PlayerCamera;
+    private GameObject CurrentPlacingTower;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CurrentPlacingTower != null)
+        {
+            Ray camray = PlayerCamera.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(camray, out RaycastHit hitInfo, 100f))
+            {
+                CurrentPlacingTower.transform.position = hitInfo.point;
+            }
+
+            if(Input.GetMouseButtonDown(0)) 
+            { 
+                CurrentPlacingTower = null;
+            }
+        }
+    }
+
+    public void SetTowerToPlace(GameObject tower)
+    {
+        CurrentPlacingTower = Instantiate(tower, Vector3.zero, Quaternion.identity);
+    }
+}
+
+
